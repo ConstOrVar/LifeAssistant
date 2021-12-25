dfc3383165af217404055538dcad9d25f0a6cb7f
ConstOrVar <ConstOrVar@users.noreply.github.com>
2021-12-25 22:11:52 +0300

    Setup StrictMode for debug builds (#4)

--- app/src/debug/java/ru/komar/lifeassistant/DebugApp.kt
+++ app/src/debug/java/ru/komar/lifeassistant/DebugApp.kt
@@ -1,12 +1,32 @@
 package ru.komar.lifeassistant
 
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import leakcanary.LeakCanary
 
 internal open class DebugApp : App() {
 
     override fun onCreate() {
+        setupStrictMode()
+
         super.onCreate()
 
         LeakCanary.config = LeakCanary.config.copy(requestWriteExternalStoragePermission = true)
+    }
+
+    private fun setupStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
     }
 }
